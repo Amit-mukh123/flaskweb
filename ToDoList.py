4475ae69bd9f0bb8c76b351da8d6ca8d8436cf2b
Amit Mukherjee <amitmukherjee78446@gmail.com>
2025-02-04 16:43:44 +0530

    Your commit message

--- ToDoList.py
+++ ToDoList.py
@@ -6,6 +6,10 @@ from flask_bcrypt import Bcrypt
 import psycopg2
 import json
 import os,time
+from flask_migrate import Migrate
+
+
+
 
 
 with open('config.json') as f:
@@ -13,9 +17,13 @@ with open('config.json') as f:
 app=Flask(__name__,template_folder='template')
 bcrypt=Bcrypt(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://amit_pg_db_user:KGYGuoNXiIuMtnrxza67pnGDYG3GF6V3@dpg-cufpd8q3esus73e31b40-a/amit_pg_db"
+app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://amit_pg_db_user:KGYGuoNXiIuMtnrxza67pnGDYG3GF6V3@dpg-cufpd8q3esus73e31b40-a.oregon-postgres.render.com/amit_pg_db"
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db_url ="postgresql://amit_pg_db_user:KGYGuoNXiIuMtnrxza67pnGDYG3GF6V3@dpg-cufpd8q3esus73e31b40-a.oregon-postgres.render.com/amit_pg_db"
+
 
 #generate secret key for session
 import secrets
@@ -42,18 +50,19 @@ mail = Mail(app)
 
 
 db=SQLAlchemy(app)
+migrate = Migrate(app, db)
 class task(db.Model):
-    srno=db.Column(db.Integer,primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     task_name=db.Column(db.String[5000],nullable=False)
-    username=db.Column(db.String[5000],nullable=False)
+    username=db.Column(db.String[5000],nullable=True)
     email=db.Column(db.String[5000],nullable=False)
     priority=db.Column(db.String[500],nullable=False)
     date=db.Column(db.DateTime,default=datetime.utcnow)
 
 
 #user login database
-class user(db.Model):
-    srno=db.Column(db.Integer,primary_key=True)
+class users(db.Model):
+    srno = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username=db.Column(db.String[200],nullable=False)
     email=db.Column(db.String[200],nullable=False)
     password=db.Column(db.String[200],nullable=False)
@@ -62,19 +71,19 @@ class user(db.Model):
 
 #send mail after a time
 def send_mail_hours(hours,task_name,user_mail):
-    time.sleep(int(hours))
+    time.sleep(int(hours)*3600)
     msg=f"Reminder: Your task '{task_name}' is still pending. Please complete it."
     for i in range(1,25):
         send_mail(user_mail,"Task Reminder",msg)
-        time.sleep(10)
+        time.sleep(3600)
 
 
 
 
 #database connection
-def get_db_connection():
+def get_db_connection(db_url):
     # Get the PostgreSQL connection details from environment variables
-    db_url ="postgresql://amit_pg_db_user:KGYGuoNXiIuMtnrxza67pnGDYG3GF6V3@dpg-cufpd8q3esus73e31b40-a/amit_pg_db"  # Assuming you set DATABASE_URL in Render environment variables
+    db_url =db_url # Assuming you set DATABASE_URL in Render environment variables
 
     # Use psycopg2 to connect to the PostgreSQL database
     conn = psycopg2.connect(db_url)
@@ -130,9 +139,9 @@ def register():
         user_password=request.form['password']
         user_email=request.form['email']
 
-        conn=get_db_connection()
+        conn=get_db_connection(db_url)
         cursor=conn.cursor()
-        cursor.execute('SELECT email FROM user WHERE email =%s',(user_email,))
+        cursor.execute('SELECT email FROM users WHERE email =%s',(user_email,))
         user_data_email=cursor.fetchone()
         print(user_data_email)
         conn.close()
@@ -144,7 +153,7 @@ def register():
 
 
         hash_password=bcrypt.generate_password_hash(user_password).decode('utf-8')
-        data=user(username=user_name,email=user_email,password=hash_password)
+        data=users(username=user_name,email=user_email,password=hash_password)
         db.session.add(data)
         db.session.commit()
         flash("Registration successful! Please verify your email.", "success")
@@ -160,14 +169,14 @@ def login():
         user_password=request.form['password']
         user_email=request.form['email']
         session['user_email']=user_email
-        conn=get_db_connection()
+        conn=get_db_connection(db_url)
         cursor=conn.cursor()
-        cursor.execute("SELECT password FROM user WHERE email = %s", (user_email,))
+        cursor.execute("SELECT password FROM users WHERE email = %s", (user_email,))
         user_data=cursor.fetchone()
         conn.close()
 
         if user_data:
-            hash_pass=user_data['password']
+            hash_pass = user_data[0]  # Accessing the first column (password)
             if bcrypt.check_password_hash(hash_pass,user_password):
                 return redirect(url_for('add_todo'))
             else:
